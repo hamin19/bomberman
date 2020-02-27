cc1c1c5d2947102cb59a4d58e57992929a2eda86
hgonzaliaz <haminson.gonzaliaz@openintl.com>
2020-02-27 07:31:08 -0500

    Se implementa lógica de colisiones

--- index.js
+++ index.js
@@ -62,23 +62,69 @@ for (let rIndex = 0; rIndex < map.length; rIndex++) {
   }
 }
 
+const doOverlap = (l1, r1, l2, r2) =>
+  l1.x > r2.x - 1 || r1.x < l2.x + 1 || l1.y > r2.y - 1 || r1.y < l2.y + 1;
+
+const colition = (x, y) => {
+  debugger;
+  const width = 18;
+  const height = 24;
+  const yHeight = y + height;
+  const xWidth = x + width;
+
+  for (let rIndex = 0; rIndex < map.length; rIndex++) {
+    const row = map[rIndex];
+
+    for (let cIndex = 0; cIndex < row.length; cIndex++) {
+      const colm = row[cIndex];
+
+      if (colm !== " ") {
+        debugger;
+        const blockX = cIndex * width;
+        const blockY = rIndex * height;
+        const blockXWidth = blockX + width;
+        const blockYHeight = blockY + height;
+        //   l1 = x, y
+        //   r1 = xWidth, yHeight
+        //   l2 = blockX, blockY
+        //   l2 = blockXWidth, blockYHeight
+
+        const overlap = !doOverlap(
+          { x, y },
+          { x: xWidth, y: yHeight },
+          { x: blockX, y: blockY },
+          { x: blockXWidth, y: blockYHeight }
+        );
+        if (overlap) return true;
+      }
+    }
+  }
+  return false;
+};
+
 document.body.addEventListener("keydown", event => {
+  let newX, newY;
   switch (event.key) {
     case "ArrowUp":
-      config.y -= 6;
       config.direction = 0;
+      newY = config.y - 2;
+      debugger;
+      if (!colition(config.x, newY)) config.y = newY;
       break;
     case "ArrowDown":
-      config.y += 6;
       config.direction = 2;
+      newY = config.y + 2;
+      if (!colition(config.x, newY)) config.y = newY;
       break;
     case "ArrowLeft":
-      config.x -= 6;
       config.direction = 1;
+      newX = config.x - 2;
+      if (!colition(newX, config.y)) config.x = newX;
       break;
     case "ArrowRight":
-      config.x += 6;
       config.direction = 3;
+      newX = config.x + 2;
+      if (!colition(newX, config.y)) config.x = newX;
       break;
   }
 });
@@ -94,4 +140,4 @@ setInterval(() => {
   player1.style.setProperty("--y", config.y);
   player1.style.setProperty("--frame", config.frame);
   player1.style.setProperty("--direction", config.direction);
-}, 250);
+}, 150);
